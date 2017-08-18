66c339fcc369f544d723478d69dce30cf4d0fe49
Shweta Herlekar <shweta_herlekar@persistent.co.in>
2017-08-18 18:13:01 +0530

    webhook

---
+++ src/main/java/com/example/MyWebhookServlet.java
@@ -0,0 +1,33 @@
+package com.example;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.json.simple.JSONObject;
+
+// [START example]
+@SuppressWarnings("serial")
+public class MyWebhookServlet extends HttpServlet {
+
+  @Override
+  public void doGet(HttpServletRequest req, HttpServletResponse resp) throws IOException {
+    PrintWriter out = resp.getWriter();
+    out.println("Hello, world");
+    
+  }
+
+  @Override
+  public void doPost(HttpServletRequest req, HttpServletResponse resp) throws IOException {
+    response.setContentType("application/json");
+    PrintWriter out = resp.getWriter();
+    JSONObject obj = new JSONObject();
+    obj.put("displayText", "500 Rs.");
+    out.println(obj);
+    
+  }
+
+}
