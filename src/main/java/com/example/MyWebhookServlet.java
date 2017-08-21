be572ca279bb23d5a40705ec811997d825bbb209
Shweta Herlekar <shweta_herlekar@persistent.co.in>
2017-08-21 16:10:21 +0530

    webhook

--- src/main/java/com/example/MyWebhookServlet.java
+++ src/main/java/com/example/MyWebhookServlet.java
@@ -7,7 +7,7 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import org.json.simple.JSONObject;
-import org.json.simple.JSONParser;
+import org.json.simple.parser.JSONParser;
 
 // [START example]
 @SuppressWarnings("serial")
