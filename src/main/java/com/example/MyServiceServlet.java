7ba65d28f5d94d865911a647bd240c1b3d8d8277
Shweta Herlekar <shweta_herlekar@persistent.co.in>
2017-08-22 18:22:30 +0530

    webhook

--- src/main/java/com/example/MyServiceServlet.java
+++ src/main/java/com/example/MyServiceServlet.java
@@ -8,39 +8,29 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
-import ai.api.AIConfiguration;
-import ai.api.AIDataService;
-import ai.api.AIServiceContext;
-import ai.api.AIServiceContextBuilder;
 import ai.api.AIServiceException;
-import ai.api.model.AIRequest;
 import ai.api.model.AIResponse;
+import ai.api.web.AIServiceServlet;
 
 
 
 // [START example]
 @SuppressWarnings("serial")
-public class MyServiceServlet extends HttpServlet {
+public class MyServiceServlet extends AIServiceServlet {
 
-	private AIDataService aiDataService;
-	public String PARAM_API_AI_KEY = "apiaiKey";
-	
   @Override
   public void doGet(HttpServletRequest req, HttpServletResponse resp) throws IOException {
     /*PrintWriter out = resp.getWriter();
     out.println("Hello Servlet!!");*/
     
-	ServletConfig conf=getServletConfig();
+	//ServletConfig conf=getServletConfig();
 	
 	String sessionId = req.getParameter("sessionId");
 	try{
 	
-	AIConfiguration aiConfig = new AIConfiguration(conf.getInitParameter(PARAM_API_AI_KEY));
-	aiDataService = new AIDataService(aiConfig);
-	
-	AIResponse aiResponse = request(req.getParameter("query"), sessionId);
-	resp.setContentType("text/plain");
-    resp.getWriter().append(aiResponse.getResult().getFulfillment().getSpeech());
+		AIResponse aiResponse = request(request.getParameter("query"), sessionId);
+		response.setContentType("text/plain");
+		response.getWriter().append(aiResponse.getResult().getFulfillment().getSpeech());
 	}
 	
 	catch(Exception e){
@@ -48,7 +38,7 @@ public class MyServiceServlet extends HttpServlet {
 	}
   }
   
-  public AIResponse request(AIRequest aiRequest, HttpSession session)
+  /*public AIResponse request(AIRequest aiRequest, HttpSession session)
       throws AIServiceException {
     return request(aiRequest,
         (session != null) ? AIServiceContextBuilder.buildFromSessionId(session.getId()) : null);
@@ -67,6 +57,6 @@ public class MyServiceServlet extends HttpServlet {
 	public AIResponse request(String query, String sessionId) throws AIServiceException {
     return request(new AIRequest(query),
         (sessionId != null) ? AIServiceContextBuilder.buildFromSessionId(sessionId) : null);
-	}
+	}*/
   
 }
