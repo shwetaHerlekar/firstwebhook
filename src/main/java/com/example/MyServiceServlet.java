a633f3e306fa8d1491a1f01f6a147657212c2ef2
Shweta Herlekar <shweta_herlekar@persistent.co.in>
2017-08-22 12:10:40 +0530

    webhook

--- src/main/java/com/example/MyServiceServlet.java
+++ src/main/java/com/example/MyServiceServlet.java
@@ -1,8 +1,7 @@
-import java.io.IOException;
+package com.example;
 
-/*import javax.servlet.ServletException;
-import javax.servlet.annotation.WebInitParam;
-import javax.servlet.annotation.WebServlet;*/
+import java.io.*;
+
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -11,23 +10,14 @@ import ai.api.AIServiceException;
 import ai.api.model.AIResponse;
 import ai.api.web.AIServiceServlet;
 
-/*@WebServlet(urlPatterns = {"/ai"},
-    initParams = {
-        @WebInitParam(name = MyServiceServlet.PARAM_API_AI_KEY,
-        value = "36e0d78753284c17ada1711f2dc6fc25")
-    })*/
+// [START example]
+@SuppressWarnings("serial")
 public class MyServiceServlet extends AIServiceServlet {
-  /**
-   * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
-   */
-  protected void doGet(HttpServletRequest request, HttpServletResponse response)
-      throws IOException {
-    try {
-      AIResponse aiResponse = request(request.getParameter("query"), request.getSession());
-      response.setContentType("text/plain");
-      response.getWriter().append(aiResponse.getResult().getFulfillment().getSpeech());
-    } catch (AIServiceException e) {
-      e.printStackTrace();
-    }
+
+  @Override
+  public void doGet(HttpServletRequest req, HttpServletResponse resp) throws IOException {
+    PrintWriter out = resp.getWriter();
+    out.println("Hello Servlet!!");
+    
   }
 }
