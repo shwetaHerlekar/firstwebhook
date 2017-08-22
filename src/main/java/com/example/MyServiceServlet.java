032d88e95437eb3957a5e523cf484fc076121318
Shweta Herlekar <shweta_herlekar@persistent.co.in>
2017-08-22 13:00:50 +0530

    webhook

--- src/main/java/com/example/MyServiceServlet.java
+++ src/main/java/com/example/MyServiceServlet.java
@@ -5,14 +5,21 @@ import java.io.*;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
+import ai.api.AIConfiguration;
+import ai.api.AIDataService;
+import ai.api.AIServiceContext;
+import ai.api.AIServiceContextBuilder;
 import ai.api.AIServiceException;
+import ai.api.model.AIRequest;
 import ai.api.model.AIResponse;
-import ai.api.web.AIServiceServlet;
+
+
 
 // [START example]
 @SuppressWarnings("serial")
-public class MyServiceServlet extends AIServiceServlet {
+public class MyServiceServlet extends HttpServlet {
 
   @Override
   public void doGet(HttpServletRequest req, HttpServletResponse resp) throws IOException {
