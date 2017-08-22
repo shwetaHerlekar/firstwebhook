e7277862d8d74602c802dd6540bdd5fa06f44370
Shweta Herlekar <shweta_herlekar@persistent.co.in>
2017-08-22 12:05:21 +0530

    webhook

--- src/main/java/com/example/MyServiceServlet.java
+++ src/main/java/com/example/MyServiceServlet.java
@@ -1,7 +1,7 @@
 import java.io.IOException;
 
-import javax.servlet.ServletException;
-/*import javax.servlet.annotation.WebInitParam;
+/*import javax.servlet.ServletException;
+import javax.servlet.annotation.WebInitParam;
 import javax.servlet.annotation.WebServlet;*/
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
