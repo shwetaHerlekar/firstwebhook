7104fdbc56c70ae562c88536a3d54cc79ee249f2
Shweta Herlekar <shweta_herlekar@persistent.co.in>
2017-08-22 12:06:51 +0530

    webhook

--- src/main/java/com/example/MyServiceServlet.java
+++ src/main/java/com/example/MyServiceServlet.java
@@ -21,7 +21,7 @@ public class MyServiceServlet extends AIServiceServlet {
    * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
    */
   protected void doGet(HttpServletRequest request, HttpServletResponse response)
-      throws ServletException, IOException {
+      throws IOException {
     try {
       AIResponse aiResponse = request(request.getParameter("query"), request.getSession());
       response.setContentType("text/plain");
