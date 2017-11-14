d047953b5e63154ca317cb4cb5af2808542d63dc
Shweta Herlekar <shweta_herlekar@persistent.co.in>
2017-11-14 09:21:06 +0530

    test

--- src/main/java/com/example/MyWebhookServlet.java
+++ src/main/java/com/example/MyWebhookServlet.java
@@ -1,6 +1,7 @@
 package com.example;
 
 import java.io.*;
+import java.util.Calendar;
 
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
@@ -20,7 +21,8 @@ public class MyWebhookServlet extends HttpServlet {
     
   }
 
-  @Override
+  @SuppressWarnings("unchecked")
+@Override
   public void doPost(HttpServletRequest req, HttpServletResponse resp) throws IOException {
     
 	try
@@ -42,20 +44,20 @@ public class MyWebhookServlet extends HttpServlet {
 	
 	
 
-	if(action1.equals("calculate_bill")){
-		String coke = String.valueOf(parameters.get("coke"));
+	if(action1.equals("QUERY_LEAVE")){
+		/*String coke = String.valueOf(parameters.get("coke"));
 		int cokecnt = Integer.parseInt(String.valueOf(parameters.get("cokecount")));
 		String pizza = String.valueOf(parameters.get("pizza"));
 		int pizzacnt = Integer.parseInt(String.valueOf(parameters.get("pizzaCount")));
 	
-		int bill = calculateBill(pizza,pizzacnt,coke,cokecnt);
+		int bill = calculateBill(pizza,pizzacnt,coke,cokecnt);*/
 		PrintWriter out = resp.getWriter();
 		JSONObject obj = new JSONObject();
-		obj.put("displayText", "Your bill is "+String.valueOf(bill)+"Rupees. Thanks for visiting us.");
-		obj.put("speech", "Your bill is "+String.valueOf(bill)+"Rupees. Thanks for visiting us.");
+		obj.put("displayText", "Your birthday is coming on 21st November 2017. Want to go out??");
+		obj.put("speech", "Your birthday is coming on 21st November 2017. Want to go out??");
 		out.println(obj);
 	}
-	else if(action1.equals("calculate_pizza_bill")){
+	/*else if(action1.equals("calculate_pizza_bill")){
 		
 		String pizza = String.valueOf(parameters.get("pizza"));
 		int pizzacnt = Integer.parseInt(String.valueOf(parameters.get("pizzaCount")));
@@ -66,16 +68,52 @@ public class MyWebhookServlet extends HttpServlet {
 		obj.put("displayText", "Your bill is "+String.valueOf(bill)+"Rupees. Thanks for visiting us.");
 		obj.put("speech", "Your bill is "+String.valueOf(bill)+"Rupees. Thanks for visiting us.");
 		out.println(obj);
-	}
+	}*/
 	
 	
 	}
 	catch(Exception e){
+		
 	}
     
   }
   
-  public int calculateBill(String pizza, int cnt1, String coke, int cnt2)
+  public JSONObject getHolidays(){
+	  int leaveBalance = 4;
+		
+		JSONObject responseObject = new JSONObject();
+		
+		Calendar bday = Calendar.getInstance();
+		bday.set(2017, 11, 21);
+		responseObject.put("birthday", bday.toString());
+		
+		JSONObject holidays = new JSONObject();
+		Calendar event_date= Calendar.getInstance();
+		event_date.set(2017, 12, 25);
+		holidays.put(event_date.toString(), "christmas");
+		
+
+		event_date= Calendar.getInstance();
+		event_date.set(2017, 12, 31);
+		holidays.put(event_date.toString(), "newyear");
+		
+		responseObject.put("holidays", holidays);
+		responseObject.put("leave_balance", leaveBalance);
+		return responseObject;
+	  
+  }
+  
+  public static boolean isEventWithinRange(Calendar testDate) {
+		Calendar today = Calendar.getInstance();
+		today.clear(Calendar.HOUR); today.clear(Calendar.MINUTE); today.clear(Calendar.SECOND);
+		
+		Calendar lastday = Calendar.getInstance();
+		lastday.add(Calendar.MONTH, 3);
+		
+	    return testDate.after(today) && testDate.before(lastday);
+	}
+  
+  /*public int calculateBill(String pizza, int cnt1, String coke, int cnt2)
   {
 	String[] pizzas = {"Austrelian", "Autumn", "cheese", "Peppy Panir"};
 	int[] cost1 = {200, 100, 300, 230};
@@ -118,6 +156,6 @@ public class MyWebhookServlet extends HttpServlet {
 	
 	return price1*cnt1;
 	
-  }
+  }*/
 
 }
