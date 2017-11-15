7f561710acd577c4d7815400f3967ce54923e58f
Shweta Herlekar <shweta_herlekar@persistent.co.in>
2017-11-15 15:29:33 +0530

    webhook

--- src/main/java/com/example/MyWebhookServlet.java
+++ src/main/java/com/example/MyWebhookServlet.java
@@ -117,9 +117,7 @@ public class MyWebhookServlet extends AIWebhookServlet {
 				outParameters.put("event", noOfDays);
 				
 				String bday = holidayData.get("birthday").toString();
-				Date birthday = new SimpleDateFormat("dd/MM/yyyy").parse(bday);
-	
-				JsonElement startDate = new JsonPrimitive(birthday.toString());
+				JsonElement startDate = new JsonPrimitive(bday);
 				outParameters.put("startDate", startDate);
 				
 				AIOutputContext contextOut = new AIOutputContext();
